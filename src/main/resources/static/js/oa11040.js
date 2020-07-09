b68b25566fc0d083777a3403a7b47afc29f3127c
斎藤隆次 <ry-saito@den.jagunma.net>
2020-07-09 09:29:06 +0900

    モック修正

--- src/main/resources/static/js/oa11040.js
+++ src/main/resources/static/js/oa11040.js
@@ -13,6 +13,7 @@ function oaex_mode_onChange() {
 	let modeArray = document.getElementsByName("mode");
 	let modeTitle = document.getElementById("mode_title");
 	let passwordOldRow = document.getElementById("password_old_row");
+	let passwordNewLabel = document.getElementById("password_new_label");
 	
 	for (let mode of modeArray) {
 		if (mode.checked) {
@@ -21,16 +22,19 @@ function oaex_mode_onChange() {
 				modeTitle.innerText = "初期パスワードの入力"
 				// passwordOldRow.style.visibility = "visible";
 				passwordOldRow.style.display = "none";
+				passwordNewLabel.innerText = "パスワード"
 				break;
 			case "1":
 				modeTitle.innerText = "パスワードのリセット"
 				// passwordOldRow.style.visibility = "hidden";
 				passwordOldRow.style.display = "none";
+				passwordNewLabel.innerText = "パスワード"
 				break;
 			case "2":
 				modeTitle.innerText = "パスワードの変更"
 				// passwordOldRow.style.visibility = "hidden";
 				passwordOldRow.style.display = "block";
+				passwordNewLabel.innerText = "新しいパスワード"
 				break;
 			default:
 				break;
