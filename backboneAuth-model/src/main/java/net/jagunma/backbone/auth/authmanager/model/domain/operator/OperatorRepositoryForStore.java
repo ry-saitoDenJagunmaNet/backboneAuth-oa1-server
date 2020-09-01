26f306976ef33af5acea2dad7afdbe5c934fdd6b
小林崇嗣 <t-kobayashi@den.jagunma.net>
2020-09-01 17:58:55 +0900

    modelからEntity非参照準備

--- backboneAuth-model/src/main/java/net/jagunma/backbone/auth/authmanager/model/domain/operator/OperatorRepositoryForStore.java
+++
@@ -1,6 +0,0 @@
-package net.jagunma.backbone.auth.authmanager.model.domain.operator;
-
-public interface OperatorRepositoryForStore {
-
-//	Operator store(Himmei himmei, SeikaOperator seikaOperator);
-}
