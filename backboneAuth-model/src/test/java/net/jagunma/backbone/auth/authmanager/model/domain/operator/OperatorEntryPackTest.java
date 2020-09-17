cb89fcfcd8194b8c36d5bb3e387aaa0cadd85807
小林崇嗣 <t-kobayashi@den.jagunma.net>
2020-09-17 13:23:11 +0900

    OA11020 オペレーター登録 テストコード

---
+++ backboneAuth-model/src/test/java/net/jagunma/backbone/auth/authmanager/model/domain/operator/OperatorEntryPackTest.java
@@ -0,0 +1,81 @@
+package net.jagunma.backbone.auth.authmanager.model.domain.operator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import net.jagunma.common.tests.constants.TestSize;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+class OperatorEntryPackTest {
+
+    /**
+     * {@link OperatorEntryPack#createFrom(
+     *     String,
+     *     String,
+     *     String,
+     *     LocalDate,
+     *     LocalDate,
+     *     Long,
+     *     String,
+     *     Long,
+     *     String,
+     *     String,
+     *     String,
+     *     String)}テスト
+     *  ●パターン
+     *    正常
+     *
+     *  ●検証事項
+     *  ・modelへのセット
+     *
+     */
+    @Test
+    @Tag(TestSize.SMALL)
+    void createFrom_test() {
+        // 実行値 ＆ 期待値
+        String operatorCode = "yu123456";
+        String operatorName = "オペレーター名";
+        String mailAddress = "test@den.jagunma.net";
+        LocalDate expirationStartDate = LocalDate.of(2020, 9, 1);
+        LocalDate expirationEndDate = LocalDate.of(2020, 9, 30);
+        Long jaId = 6L;
+        String jaCode = "006";
+        Long tempoId = 1L;
+        String tempoCode = "001";
+        String changeCause = "新職員の入組による登録";
+        String password = "PaSsWoRd";
+        String confirmPassword = "PaSsWoRd";
+
+        // 実行
+        OperatorEntryPack operatorEntryPack = OperatorEntryPack.createFrom(
+            operatorCode,
+            operatorName,
+            mailAddress,
+            expirationStartDate,
+            expirationEndDate,
+            jaId,
+            jaCode,
+            tempoId,
+            tempoCode,
+            changeCause,
+            password,
+            confirmPassword);
+
+        // 結果検証
+        assertTrue(operatorEntryPack instanceof OperatorEntryPack);
+        assertThat(operatorEntryPack.getOperatorCode()).isEqualTo(operatorCode);
+        assertThat(operatorEntryPack.getOperatorName()).isEqualTo(operatorName);
+        assertThat(operatorEntryPack.getMailAddress()).isEqualTo(mailAddress);
+        assertThat(operatorEntryPack.getExpirationStartDate()).isEqualTo(expirationStartDate);
+        assertThat(operatorEntryPack.getExpirationEndDate()).isEqualTo(expirationEndDate);
+        assertThat(operatorEntryPack.getJaId()).isEqualTo(jaId);
+        assertThat(operatorEntryPack.getJaCode()).isEqualTo(jaCode);
+        assertThat(operatorEntryPack.getTempoId()).isEqualTo(tempoId);
+        assertThat(operatorEntryPack.getTempoCode()).isEqualTo(tempoCode);
+        assertThat(operatorEntryPack.getChangeCause()).isEqualTo(changeCause);
+        assertThat(operatorEntryPack.getPassword()).isEqualTo(password);
+        assertThat(operatorEntryPack.getConfirmPassword()).isEqualTo(confirmPassword);
+    }
+}
