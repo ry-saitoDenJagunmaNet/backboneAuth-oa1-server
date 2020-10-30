3c768982dbda3fdbc2aca70be5e1e0608b2dc46a
小林崇嗣 <t-kobayashi@den.jagunma.net>
2020-10-30 15:57:18 +0900

    パスワード格納(PasswordHistoryForStoreDataSource)

--- backboneAuth-oa1-infra/src/main/java/net/jagunma/backbone/auth/authmanager/infra/datasource/passwordHistory/PasswordHistoryForStoreDataSource.java
+++ backboneAuth-oa1-infra/src/main/java/net/jagunma/backbone/auth/authmanager/infra/datasource/passwordHistory/PasswordHistoryForStoreDataSource.java
@@ -1,9 +1,7 @@
 package net.jagunma.backbone.auth.authmanager.infra.datasource.passwordHistory;
 
-import java.time.LocalDateTime;
 import net.jagunma.backbone.auth.authmanager.model.domain.passwordHistory.PasswordHistory;
 import net.jagunma.backbone.auth.authmanager.model.domain.passwordHistory.PasswordHistoryRepositoryForStore;
-import net.jagunma.backbone.auth.authmanager.model.types.PasswordChangeType;
 import net.jagunma.backbone.auth.model.dao.passwordHistory.PasswordHistoryEntity;
 import net.jagunma.backbone.auth.model.dao.passwordHistory.PasswordHistoryEntityDao;
 import org.springframework.stereotype.Component;
@@ -30,53 +28,25 @@ public class PasswordHistoryForStoreDataSource implements PasswordHistoryReposit
     public void store(PasswordHistory passwordHistory) {
 
         // パスワード履歴のインサートを行います
-        insertPasswordHistory(passwordHistory.getOperatorId(), passwordHistory.getPassword(), passwordHistory.getChangeDateTime(), passwordHistory.getPasswordChangeType());
+        insertPasswordHistory(passwordHistory);
     }
 
     /**
      * パスワード履歴のインサートを行います
      *
-     * @param operatorId オペレーターID
-     * @param password パスワード
-     * @param changeDateTime 変更日時
-     * @param passwordChangeType 変更種別
+     * @param passwordHistory パスワード履歴
      * @return パスワード履歴エンティティ
      */
-    PasswordHistoryEntity insertPasswordHistory(Long operatorId, String password, LocalDateTime changeDateTime, PasswordChangeType passwordChangeType) {
+    PasswordHistoryEntity insertPasswordHistory(PasswordHistory passwordHistory) {
         PasswordHistoryEntity passwordHistoryEntity = new PasswordHistoryEntity();
 
-        passwordHistoryEntity.setOperatorId(operatorId);
-        passwordHistoryEntity.setChangeDateTime(changeDateTime);
-        passwordHistoryEntity.setPassword(password);
-        passwordHistoryEntity.setChangeType(passwordChangeType.getCode());
+        passwordHistoryEntity.setOperatorId(passwordHistory.getOperatorId());
+        passwordHistoryEntity.setChangeDateTime(passwordHistory.getChangeDateTime());
+        passwordHistoryEntity.setPassword(passwordHistory.getPassword());
+        passwordHistoryEntity.setChangeType(passwordHistory.getPasswordChangeType().getCode());
 
         passwordHistoryEntityDao.insert(passwordHistoryEntity);
 
         return passwordHistoryEntity;
     }
-
-//    /**
-//     * パスワードの格納を行います
-//     * （初期パスワードの入力）
-//     *
-//     * @param operatorId オペレーターID
-//     * @param password パスワード
-//     * @param changeDateTime 変更日時
-//     */
-//    public void store(Long operatorId, String password, LocalDateTime changeDateTime) {
-//        store(operatorId, password, changeDateTime, PasswordChangeType.初期);
-//    }
-//    /**
-//     * パスワードの格納を行います
-//     *
-//     * @param operatorId オペレーターID
-//     * @param password パスワード
-//     * @param changeDateTime 変更日時
-//     * @param passwordChangeType 変更種別
-//     */
-//    public void store(Long operatorId, String password, LocalDateTime changeDateTime, PasswordChangeType passwordChangeType) {
-//        // パスワード履歴のインサートを行います
-//        insertPasswordHistory(operatorId, password, changeDateTime, passwordChangeType);
-//    }
-
 }
