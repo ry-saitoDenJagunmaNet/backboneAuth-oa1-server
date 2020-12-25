fa1ee6ead2b27b6495b7568f85849254e38a7ba6
小林崇嗣 <t-kobayashi@den.jagunma.net>
2020-12-25 11:03:45 +0900

    OA11050 取引ロール付与（実装）

--- backboneAuth-oa1-service/src/main/resources/static/js/oa11050.js
+++ backboneAuth-oa1-service/src/main/resources/static/js/oa11050.js
@@ -1,35 +1,35 @@
 /**
-* サブシステムフィルタセレクトチェンジイベントです。
+* サブシステムフィルタ SelectChangeイベントです。
 * 未付与ロールテーブルより下記のロールを非表示
 *  ・選択されたサブシステムに該当しないロール
 *  ・既に付与ロールテーブルに存在するロール
 */
 function oaex_subsystemFilter_onChange() {
-	let grantedRoleTable = document.getElementById("granted_role_table");
-	let notGrantedRoleTable = document.getElementById("not_granted_role_table");
+	let allocateRoleTable = document.getElementById("allocate_role_table");
+	let unallocateRoleTable = document.getElementById("unallocate_role_table");
 	let subsystemFilter = document.getElementById("subsystem_filter");
 
 	// 全ロール表示
-	for (let notGrantedRoleTableRow of notGrantedRoleTable.rows) {
-		notGrantedRoleTableRow.classList.remove(_RIGHT_TABLE_HIDDEN);
+	for (let unallocateRoleTableRow of unallocateRoleTable.rows) {
+		unallocateRoleTableRow.classList.remove(_RIGHT_TABLE_HIDDEN);
 	}
 
 	// 既に付与ロールテーブルに存在するロールを取得
-	let grantedRoleCodeArray = [];
-	for (let grantedRoleTableRow of grantedRoleTable.rows) {
-		if (grantedRoleTableRow.cells[0].innerText != "") {
-			grantedRoleCodeArray.push(grantedRoleTableRow.cells[0].innerText)
+	let allocateRoleCodeArray = [];
+	for (let allocateRoleTableRow of allocateRoleTable.rows) {
+		if (allocateRoleTableRow.cells[0].innerText != "") {
+			allocateRoleCodeArray.push(allocateRoleTableRow.cells[0].innerText)
 		}
 	}
 
 	// フィルタ対象かつ付与ロールテーブルに存在しないロールを表示
 	if (subsystemFilter.value != "") {
-		for (let notGrantedRoleTableRow of notGrantedRoleTable.rows) {
-			if (notGrantedRoleTableRow.cells[0].innerText.substring(0,2) == subsystemFilter.value &&
-				grantedRoleCodeArray.includes(notGrantedRoleTableRow.cells[0].innerText) == false) {
-					notGrantedRoleTableRow.classList.remove(_RIGHT_TABLE_HIDDEN);
+		for (let unallocateRoleTableRow of unallocateRoleTable.rows) {
+			if (unallocateRoleTableRow.cells[0].innerText.substring(0,2) == subsystemFilter.value &&
+					allocateRoleCodeArray.includes(unallocateRoleTableRow.cells[0].innerText) == false) {
+				unallocateRoleTableRow.classList.remove(_RIGHT_TABLE_HIDDEN);
 			} else {
-				notGrantedRoleTableRow.classList.add(_RIGHT_TABLE_HIDDEN)
+				unallocateRoleTableRow.classList.add(_RIGHT_TABLE_HIDDEN)
 			}
 		}
 	}
